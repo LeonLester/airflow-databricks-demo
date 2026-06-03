7fe13e3666b26bedb41b02cea185ff91add122e1
Leonidas Karagiannis <leonidask914@gmail.com>
2026-06-03 19:04:34 +0200

    separate environments in config

--- airflow-databricks/dags/config.py
+++ airflow-databricks/dags/config.py
@@ -1,6 +1,16 @@
+from airflow.models import Variable
+
 DATABRICKS_CONN_ID = "databricks_default"
 
-JOB_IDS = {
-    "bronze_ingest": 421658606944721,
-    "silver_transform": 921960675949165,
+
+_JOB_IDS = {
+    "dev": {
+        "bronze_ingest": 421658606944721,
+        "silver_transform": 921960675949165,
+    },
+
 }
+
+def get_job_id(job_name: str) -> int:
+    env = Variable.get("env", default_var="dev")
+    return _JOB_IDS[env][job_name]
--- airflow-databricks/dags/etl_pipeline.py
+++ airflow-databricks/dags/etl_pipeline.py
@@ -1,11 +1,11 @@
 from datetime import datetime
 from airflow import DAG
 from airflow.providers.databricks.operators.databricks import DatabricksRunNowOperator
-from config import DATABRICKS_CONN_ID, JOB_IDS
+from config import DATABRICKS_CONN_ID, get_job_id
 
 with DAG(
     dag_id="etl_pipeline",
-    start_date=datetime(2024, 1, 1),
+    start_date=datetime(2026, 6, 3),
     schedule=None,
     catchup=False,
 ) as dag:
@@ -13,13 +13,13 @@ with DAG(
     bronze_ingest = DatabricksRunNowOperator(
         task_id="bronze_ingest",
         databricks_conn_id=DATABRICKS_CONN_ID,
-        job_id=JOB_IDS["bronze_ingest"],
+        job_id=get_job_id("bronze_ingest"),
     )
 
     silver_transform = DatabricksRunNowOperator(
         task_id="silver_transform",
         databricks_conn_id=DATABRICKS_CONN_ID,
-        job_id=JOB_IDS["silver_transform"],
+        job_id=get_job_id("silver_transform"),
     )
 
     bronze_ingest >> silver_transform
